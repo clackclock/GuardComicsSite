584d3b1659f318206f82386485aa8e37d374f408
clackclock <clackclick2020@gmail.com>
2024-08-06 15:44:05 -0400

    lol

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
+
+    $(".menu").on('click', function(){
+        $(".pageGuide").show("fast");
+    })
+    $(".menu").on('dblclick', function(){
+        $(".pageGuide").hide("fast");
+    })
+})
 
