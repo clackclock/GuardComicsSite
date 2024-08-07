9fa7277e4e43a84ab1b4f5bb86fdb89a82781228
clackclock <clackclick2020@gmail.com>
2024-08-07 16:57:58 -0400

    update on design layout
    a lot of general buttons have been placed and made pretty

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
 
-    $(".menu").on('click', function(){
-        $(".pageGuide").show("fast");
-    })
-    $(".menu").on('dblclick', function(){
-        $(".pageGuide").hide("fast");
-    })
 })
 
