a56c66b9268ee8de7d54a2d47e34550d72d6c07d
clackclock <clackclick2020@gmail.com>
2024-08-13 20:50:15 -0400

    name edit

--- scripts/comReader.js
+++ scripts/comReader.js
@@ -1,2 +1,6 @@
 import React from 'react';
 
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
