b8c2498eb1c35aecfbbe13551b0f299e8d7fd3f0
clackclock <clackclick2020@gmail.com>
2024-12-15 12:59:48 -0500

    switching over the index

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-var siteWidth = 1280;
-var scale = screen.width /siteWidth;
-
-$(document).ready(function(){
-
-    document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
-});
--- scripts/userInfoTest.sql
+++
@@ -1,4 +0,0 @@
-create TABLE UserPass(
-    Name VARCHAR(255),
-    Password VARCHAR(255)
-)
