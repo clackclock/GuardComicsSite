903a818645d63cb95336ab35f217fd9e198b9bb9
clackclock <clackclick2020@gmail.com>
2025-05-28 20:14:08 -0400

    started the index page and started the files for the gallery
    i finally figured out how this works... maybe
    but html can be put in the the typescript bits and js can happen in the same page or on the html if ur a coward. i am but meh

--- gcu/src/App.jsx
+++ gcu/src/App.jsx
@@ -6,17 +6,36 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return <div className="fullscreen-bg"> 
     <>
-      <div>
+      <header>
+        <img className = "logo" src = "./src/assets/orionthemes-placeholder-image-2.png" alt = "gc_logo" height = "150px"/>
+        <div id = "title">GUARD COMICS</div>
+
+        <div className = "dropdown">
+          <img className ="menu" src = "https://upload.wikimedia.org/wikipedia/commons/d/db/MenuResp.png" width = "60px" height = "60px" />
+          <div className = "dropdown-content">
+            <button className="button-57" role="button"><span class="text">Artisans' Quarters</span><span>Comic List</span></button>
+            <button className="button-57" role="button"><span class="text">Town Bulletin</span><span>Latest Updates</span></button>
+            <button className="button-57" role="button"><span class="text">Spring Gala</span><span>New Comics</span></button>
+            {/* <button className="button-57" role="button"><span class="text">Genres</span><span>IDK Genres</span></button> */}
+            <button className="button-57" role="button"><span class="text">King's Court</span><span>Info & More</span></button>
+          </div>
+        </div>
+      </header>
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,9 +46,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  </div>;
 }
 
 export default App
