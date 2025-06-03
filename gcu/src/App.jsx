e6c86196ee09680d345b012012565d43406f29fb
clackclock <clackclick2020@gmail.com>
2025-06-03 16:17:50 -0400

    test

--- gcu/src/App.jsx
+++ gcu/src/App.jsx
@@ -3,9 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import placeHolder from "https://upload.wikimedia.org/wikipedia/commons/d/db/MenuResp.png";
+import ph from "../src/assets/orionthemes-placeholder-image-2";
+
 function App() {
   const [count, setCount] = useState(0)
   
+  const ranComic= {
+
+  }
+
   return <div className="fullscreen-bg"> 
     <>
       <header>
@@ -13,16 +20,23 @@ function App() {
         <div id = "title">GUARD COMICS</div>
 
         <div className = "dropdown">
-          <img className ="menu" src = "https://upload.wikimedia.org/wikipedia/commons/d/db/MenuResp.png" width = "60px" height = "60px" />
+          <img className ="menu" src = {placeHolder} alt="empty" width = "60px" height = "60px" />
           <div className = "dropdown-content">
             <button className="button-57" role="button"><span class="text">Artisans' Quarters</span><span>Comic List</span></button>
             <button className="button-57" role="button"><span class="text">Town Bulletin</span><span>Latest Updates</span></button>
-            <button className="button-57" role="button"><span class="text">Spring Gala</span><span>New Comics</span></button>
+            {/* <button className="button-57" role="button"><span class="text">Spring Gala</span><span>New Comics</span></button> */}
             {/* <button className="button-57" role="button"><span class="text">Genres</span><span>IDK Genres</span></button> */}
             <button className="button-57" role="button"><span class="text">King's Court</span><span>Info & More</span></button>
           </div>
         </div>
       </header>
+
+      <div itemID = "Artisan">
+        <div className='booklet'>
+          <img src={ph} alt="booklet cover"/>
+          <a className='title'></a>
+        </div>
+      </div>
 
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
